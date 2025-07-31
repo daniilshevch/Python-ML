7826c5060d063bdf34c49a5176d411262aa20907
daniilshevch <daniil.shevchenko.shi.2023@lpnu.ua>
2025-07-31 17:33:57 +0300

    31.07 17:33

--- Python Repeating/3.py
+++ Python Repeating/3.py
@@ -91,39 +91,111 @@ def second_part():
     print(arr12)
     arr13 = arr[::-3]
     print(arr13)
+def third_part():
+    arr = [0,1,2,3,4,5,6,7]
+    arr[0:3] = ["a", "b", "c", "d"]
+    print(arr)
+    arr[0:3] = [0,1]
+    print(arr)
+    del arr[0:5:2]
+    print(arr)
+    print(arr[1:16])
+    names = ["Jack", "Mike"]
+    print(len(names))
+    names.append("Alice")
+    print(names)
+    names.insert(1, "Bill")
+    print(names)
 
-arr = [0,1,2,3,4,5,6,7]
-arr[0:3] = ["a", "b", "c", "d"]
-print(arr)
-arr[0:3] = [0,1]
-print(arr)
-del arr[0:5:2]
-print(arr)
-print(arr[1:16])
-names = ["Jack", "Mike"]
-print(len(names))
-names.append("Alice")
-print(names)
-names.insert(1, "Bill")
-print(names)
+    names.extend(["Petro", "Poroshenko"])
+    print(names)
+    index_of_alice = names.index("Alice")
+    print(index_of_alice)
 
-names.extend(["Petro", "Poroshenko"])
-print(names)
-index_of_alice = names.index("Alice")
-print(index_of_alice)
+    names.remove("Jack")
+    print(names)
+    print("Alice" in names)
+    print("Hanna" in names)
 
-names.remove("Jack")
-print(names)
-print("Alice" in names)
-print("Hanna" in names)
+    nums = [10,20,30,40,50]
+    nums[1:4] = [11,22]
+    print(nums)
+    nums = [1,2,3,4,5,6,7,8,9]
+    nums[::2] = [0,0,0,0,0]
+    print(nums)
 
-nums = [10,20,30,40,50]
-nums[1:4] = [11,22]
-print(nums)
-nums = [1,2,3,4,5,6,7,8,9]
-nums[::2] = [0,0,0,0,0]
-print(nums)
+    print(nums.count(0))
 
-print(nums.count(0))
+    nums = [1,5,3,3,2,-1,5,8,-5, -6]
+    nums.sort(key=lambda x: -x)
+    print(nums)
+
+    filtered_nums = filter(lambda x: x % 2 == 0, nums)
+    for n in filtered_nums:
+        print(n, end = " ")
+    print()
+    def condition(x):
+        return x > 3
+    filtered_nums = filter(condition, nums)
+    for n in filtered_nums:
+        print(n, end = " ")
+    print()
+    print("--------")
+
+
+    numbers = [1,2,3,4,5]
+    squares = map(lambda p: p * p, numbers)
+    for k in squares:
+        print(k)
+def fourth_part():
+    class Person:
+        def __init__(self, name, age):
+            self.name = name
+            self.age = age
+        def __str__(self):
+            return f"Person({self.name}, {self.age})"
+        def __repr__(self):
+            return self.__str__()
+
+    people = [Person("Petro", 45), Person("Jack", 22), Person("Kate", 23)]
+
+    view = map(lambda p: p.age, people)
+    for age in view:
+        print(age, end = " ")
+    print()
+    people2 = people.copy()
+    people3 = people
+
+    people2.append(Person("XXX", 80))
+    print(people)
+
+    people3.append(Person("YYY", 81))
+    print(people)
+
+    nums1 = [1,2,3,4]
+    nums2 = [5,6,7]
+    nums3 = nums1 + nums2
+    print(nums3)
+
+    objects = [
+        ["Tom", 25],
+        ["James", "Microsoft"],
+        [True, False]
+    ]
+    for obj in objects:
+        for item in obj:
+            print(item, end = " ")
+        print()
+
+tom = ("Tom", 23)
+jack = ("Jack", 40)
+print(tom)
+print(jack)
+data = ["Karl", 37, "Microsoft"]
+karl = tuple(data)
+print(data)
+print(karl)
+
+
 
 
